04b877f763fcfaed82a226c0fe4500a7a4b66870
Ian Wood <ianwood103@gmail.com>
2021-01-26 23:10:27 -0500

    finished creation of react task tracker app with no server integration

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import AddTask from './components/AddTask'
 import { useState } from 'react';
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(false);
+
   const [tasks, setTasks] = useState(
     [
       {
@@ -27,6 +30,13 @@ function App() {
     ]
   );
 
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask]);
+  }
+
   // Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => id !== task.id));
@@ -39,8 +49,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
-      <AddTask />
+      <Header title="Task Tracker" onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : "No Tasks to Show."}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = (props) => {
-    const onClick = () => {
-        console.log("click");
-    }
-
+const Header = ({ title, onAdd, showAdd }) => {
     return (
         <header className='header'>
-            <h1>{props.title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <h1>{title}</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
     )
 }
